ae079eeb4839e1fee42e93f447f602666bcce9a9
mdantaas <mdantaas08@gmail.com>
2025-05-16 11:02:32 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("=== Bem-vindo ao Show do Milhão ===");
+        System.out.println("1. Iniciar jogo");
+        System.out.println("2. Ver ranking");
+        System.out.println("3. Sair");
+    }
+}
