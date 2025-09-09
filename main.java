f9d0661a6df76f2dd78bc6c0fe0c4c6e8ac9cc6d
mdantaas <mdantaas08@gmail.com>
2025-09-09 12:18:28 -0300

    Atualizar o main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("=== Bem-vindo ao Show do Milhão ===");
-        System.out.println("1. Iniciar jogo");
-        System.out.println("2. Ver ranking");
-        System.out.println("3. Sair");
-    }
-}
